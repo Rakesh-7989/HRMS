839f4af695aa3f53b64985380d06a0620c076dca
Rakesh-7989 <boyapatirakesh7989@gmail.com>
2026-07-10 16:34:01 +0530

    test: minimal Vercel serverless function

---
+++ backend/api/index.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({ status: 'ok', message: 'Vercel serverless works!' }));
+});
+
+module.exports = server;
